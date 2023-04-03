024fdd9256c9c8f0e38ee72b5c1a9d883d90aa08
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-04-03 21:18:14 +0900

    Fixed styles and js

--- source/js/modules/about-button/about-button.js
+++ source/js/modules/about-button/about-button.js
@@ -1,9 +1,6 @@
 const buttons = document.querySelectorAll('[data-about="button"]');
 
 export const toggleAboutButtons = () => {
-  if (!buttons) {
-    return;
-  }
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       if (!button.classList.contains('is-open')) {
